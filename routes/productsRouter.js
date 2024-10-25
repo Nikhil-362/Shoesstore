cf4d222158cb083b94112d9c15200cdb61e8cc19
R.Nikhil Reddy <122670124+Nikhil-362@users.noreply.github.com>
2024-10-25 13:29:47 +0530

    "Add Product Preview page"

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,16 +3,18 @@ const { create, update } = require("../controllers/productsController");
 const { isOwner } = require("../middleware/isOwner");
 const router = express.Router();
 const upload = require("../config/multer-config");
-
+const productsModel = require("../model/products-model");
 
 router.get("/", (req, res) => {
   res.send("products Router");
 });
 
+router.get("/preview/:id", async (req, res) => {
+  const product = await productsModel.findOne({ _id: req.params.id})
+  res.render("productPage", {product});
+});
 
-router.post("/create", upload.single("image"),isOwner, create);
-
+router.post("/create", upload.single("image"), isOwner, create);
 router.post("/update/:id", upload.single("image"), update);
-
 
 module.exports = router;
